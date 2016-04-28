8014835a041cd1ac3bbf564792684155a68c5678
Mauricio Junior <mauricio14junior@gmail.com>
2016-04-28 20:43:31 -0300

    Updated Procfile parameters

--- appheroku/appheroku/settings/production.py
+++ appheroku/appheroku/settings/production.py
@@ -3,13 +3,11 @@ from appheroku.settings.base import *
 DEBUG = False
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'sqlite3.db'),
-        #'NAME': 'olist',
-        #'USER': 'olist',
-        #'PASSWORD': 'master',
-        #'HOST': '127.0.0.1',
-        #'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'olist',
+        'NAME': 'workatolist',
+        'PASSWORD': 'master',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
