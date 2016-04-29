08529be71777eab9a7d0e3ddf9a17d1f32e289e1
Mauricio Junior <mauricio14junior@gmail.com>
2016-04-29 01:08:42 -0300

    Changed static configuration

--- appheroku/settings.py
+++ appheroku/settings.py
@@ -106,8 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'appheroku/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "appheroku/static"),
+    os.path.join(BASE_DIR, "static"),
 ]
